7e8e582b454032c70ee64796e085b695dd3d21fa
shenjianzhang <sjzhang@pku.edu.cn>
2020-05-14 12:27:42 +0800

    First version for release.
    Add files in /Example. More lines in comments of qssp2sac.py.

--- qssp2sac.py
+++ qssp2sac.py
@@ -1,6 +1,61 @@
 #!/usr/bin/env python3
 
+"""
+Convert synthetic seismograms calculated by [QSSP](ftp://ftp.gfz-potsdam.de/pub/home/turk/wang/qssp2020-code+input.rar) to SAC file format.
+
+## Features:
+1. Convert outputs of QSSP to SAC-format with one command or script;
+2. Write basic header information like location of event and station, name of event and station, and channel name;
+3. Highlight observable types and directions according to QSSP outputs in the name of SAC files.
+
+## Prerequisites
+Module `sactrace` of `obspy.io.sac` is used for SAC writing, so the tool box [ObsPy](https://github.com/obspy/obspy/wiki)
+(Beyreuther et al., 2010) is needed. See [ObsPy homepage](https://github.com/obspy/obspy/wiki#installation) for the instructions of installation.
+
+## Usage:
+```
+$ python qssp2sac.py qssp.inp qssp.out
+```
+Note
+- `qssp.inp` and `qssp.out` are in the SAME directory of `qssp2sac.py`, in the other word, they should be
+  in the current working directory; (using absolute or relative path will be added in the furture)
+- `qssp.inp` is the input file with parameters for QSSP;
+- `qssp.out` is one of several output files after running QSSP with the input file `qssp.inp`;
+- the file name of `qssp.out` should follow the format as *prefix_observable_direction.dat*, in which
+  *prefix* is set in section RECEIVER PARAMETERS in `qssp.inp`, *observable* is the one you select in
+  section RECEIVER PARAMETERS in `qssp.inp` and direction is from *e/n/z* or *ee/en/ez/nn/nz/zz* upon
+  different observables.
+- JUST REMEMBER NOT TO RENAME OUTPUT FILES OF QSSP WHEN USING THIS SCRIPT.
+
+## Example:
+Assuming you run QSSP with the input data file `qssp_exmaple.inp` and obtain 3 outputs as `example_disp_e.dat`
+`example_disp_n.dat`, and `example_disp_z.dat` (these four files could be found in folder `./Example`).
+Then you could run the command as
+```
+$ cd ./Example
+$ mv ../qssp2sac.py ./
+$ python qssp2sac.py qssp_example.inp exmaple_disp_e.dat
+QSSP input file is qssp_example.inp
+QSSP output file is example_disp_e.dat
+Create SAC file: EXAMPLE.STA1.DISP.BXE.SAC
+Create SAC file: EXAMPLE.STA2.DISP.BXE.SAC
+```
+and get two SAC files as `EXAMPLE.STA1.DISP.BXE.SAC` and `EXAMPLE.STA2.DISP.BXE.SAC`. You could check header information
+and plot waveform using program SAC (Seismic Analysis Code).
+
+### Reference
+*M. Beyreuther, R. Barsch, L. Krischer, T. Megies, Y. Behr and J. Wassermann* (2010)
+[ObsPy: A Python Toolbox for Seismology](http://www.seismosoc.org/Publications/SRL/SRL_81/srl_81-3_es/)
+SRL, 81(3), 530-533
+[DOI: 10.1785/gssrl.81.3.530](http://dx.doi.org/10.1785/gssrl.81.3.530)
+
+Written by
+Shenjian Zhang <sjzhang@pku.edu.cn>
+13 May 2020
+"""
+
 import sys
+import warnings
 import numpy as np
 from obspy.io.sac import SACTrace
 
@@ -15,7 +70,6 @@ def skipcmmt(f):
 
 def readinput(qsspin):
     ''' Read some info. from QSSP input file 'qsspin'
-    return
     '''
     fi = open(qsspin, 'r')
     # Uniform receiver depth
@@ -119,6 +173,67 @@ def readoutput(qsspout):
     seisdata = qsspdata[:,1:]    # only seismograms ignoring time
     return seisdata
 
+def datatype_channel(prfx, fname):
+    ''' Get channel code (E/N/Z) or (EE/EN/EZ/NN/NZ/ZZ) and
+    type of output observables of QSSP from the file name of QSSP output.
+    -observable code       -observabl type (in QSSP output)
+     DISP                   (disp)lacement
+     VELO                   (velo)city
+     ACCE                   (acce)leration
+     ROTA                   (rota)tion
+     ROTA.RATE              rotation rate (rota_rate)
+     STRN                   (strain)
+     STRN.RATE              strain rate (strain_rate)
+     STRS                   (stress)
+     STRS.RATE              stress rate (stress_rate)
+     GRAV                   (gravitation)
+     GRME                   (gravimeter)
+    '''
+    # Read channel(E,N,Z) from QSSP output filename
+    list3c = ['_e.dat', '_n.dat', '_z.dat']
+    list6c = ['ee.dat', 'en.dat', 'ez.dat', 'nn.dat', 'nz.dat', 'zz.dat']
+    if fname[-6:] in list3c:
+        chancode = 'BX'+fname[-5].upper()
+        if fname[-5] == 'e':
+            az = 90
+            inc = 90
+        elif fname[-5] == 'n':
+            az = 0
+            inc = 90
+        else:
+            az = 0
+            inc = 0
+    elif fname[-6:] in list6c:
+        chancode = 'B'+fname[-6:-4].upper()
+        az = -12345.0
+        inc = -12345.0
+    else:
+        chancode = 'BXX'
+        az = -12345.0
+        inc = -12345.0
+    # read type of outputs from QSSP output filename
+    listdva = ['disp', 'velo', 'acce', 'rota']
+    listsig = ['strain', 'stress']
+    if fname[len(prfx)+1:len(prfx)+5] in listdva:
+        if fname[len(prfx)+6:len(prfx)+10] == 'rate':
+            observ = fname[len(prfx)+1:len(prfx)+5].upper() + '.RATE'
+        else:
+            observ = fname[len(prfx)+1:len(prfx)+5].upper()
+    elif fname[len(prfx)+1:len(prfx)+7] in listsig:
+        observtmp = fname[len(prfx)+1:len(prfx)+4]+fname[len(prfx)+6]    # 'strn' for strain, 'strs' for stress
+        if fname[len(prfx)+8:len(prfx)+12] == 'rate':
+            observ = observtmp.upper() + '.RATE'
+        else:
+            observ = observtmp.upper()
+    elif fname[len(prfx)+1:len(prfx)+12] == 'gravitation':
+        observ = 'GRAV'
+    elif fname[len(prfx)+1:len(prfx)+11] == 'gravimeter':
+        observ = 'GRME'
+    else:
+        observ = 'UNKNOWN'
+        print('Unknown output observables of QSSP.')
+    return chancode, az, inc, observ
+
 def main():
     print('QSSP input file is', sys.argv[1])
     print('QSSP output file is', sys.argv[2])
@@ -128,34 +243,12 @@ def main():
     deltat, spara, prfx, nr, rdepth, rpara, rnames = readinput(qsspinput)
     # read seismograms from QSSP output files
     seis = readoutput(qsspoutput)
-
-    # Write SAC files
-    # read channel(E,N,Z) from QSSP output filename
-    list3c = ['_e.dat', '_n.dat', '_z.dat']
-    list6c = ['ee.dat', 'en.dat', 'ez.dat', 'nn.dat', 'nz.dat', 'zz.dat']
-    if qsspoutput[-6:] in list3c:
-        chan = 'BX'+qsspoutput[-5].upper()
-        if qsspoutput[-5] == 'e':
-            caz = 90
-            cin = 90
-        elif qsspoutput[-5] == 'n':
-            caz = 0
-            cin = 90
-        else:
-            caz = 0
-            cin = 0
-    elif qsspoutput[-6:] in list6c:
-        chan = 'B'+qsspoutput[-6:-4].upper()
-        caz = -12345.0
-        cin = -12345.0
-    else:
-        chan = 'BXX'
-        caz = -12345.0
-        cin = -12345.0
-    #
+    # read channel code and observable types
+    chan, caz, cin, datatype = datatype_channel(prfx, qsspoutput)
+    # write SAC files for each receiver/station
     for i in range(0, nr):
         # name for SAC file
-        sacname = prfx.upper() + '.' + rnames[i].upper() + '.' + chan + '.SAC'
+        sacname = prfx.upper() + '.' + rnames[i].upper() + '.' + datatype + '.' + chan + '.SAC'
         header = {'iztype': 'io', 'o': 0, 'b': rpara[i,2], 'delta': deltat, \
         'kevnm': prfx, 'evla': spara[0], 'evlo': spara[1], 'evdp': spara[2],\
         'kstnm': rnames[i], 'kcmpnm': chan, 'cmpaz': caz, 'cmpinc': cin, \
@@ -163,6 +256,7 @@ def main():
         'lcalda': True}
         tr = SACTrace(data=seis[:,i], **header )
         tr.write(sacname)
+        print('Create SAC file:', sacname)
 
 if __name__ == '__main__':
     main()
